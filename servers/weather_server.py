f2926e3f14a8a9ef6dceca03cf8c4a2ab7108dc9
Agniva <dastidaragnivo@gmail.com>
2026-07-22 22:39:23 +0530

    setting up multiple mcp servers

---
+++ servers/weather_server.py
@@ -0,0 +1,15 @@
+# weather_server.py
+# Transport protocol: sse (server-sent-events = remote client connects to server via HTTP)
+
+from typing import List
+from mcp.server.fastmcp import FastMCP
+
+mcp = FastMCP("Weather")
+
+@mcp.tool()
+async def get_weather(location: str) -> str:
+    """Get weather for location."""
+    return "It's always sunny in New York"
+
+if __name__ == "__main__":
+    mcp.run(transport="sse")
