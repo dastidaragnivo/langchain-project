3a558799154eafe2a4f79de838ea23660292e336
Agniva <dastidaragnivo@gmail.com>
2026-07-23 10:15:19 +0530

    multimcpserver setup

--- main.py
+++ main.py
@@ -2,7 +2,21 @@ import asyncio
 from dotenv import load_dotenv
 load_dotenv()  # Load environment variables from .env file
 import os
-print(os.getenv("LANGSMITH_PROJECT"))  # Print the value of LANGSMITH_PROJECT
+
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
+
+from langchain_anthropic import ChatAnthropic
+from langchain_mcp_adapters.tools import load_mcp_tools
+from langchain.agents import create_agent
+
+llm = ChatAnthropic(model="claude-sonnet-4-6", temperature=0, api_key=os.getenv("ANTHROPIC_API_KEY"))
+
+stdio_server_params = StdioServerParameters(
+    command="python",
+    args=["C:\\Users\\dasti\\Python & Data Science\\langchain-course\\mcp-servers\\mcp-crash-course\\servers\\math_server.py"],
+    server_description="A simple math server that can add and multiply numbers.",
+)
 
 async def main():
     print("Hello from mcp-crash-course!")
